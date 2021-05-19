7c0b8bf4cec49a5d19688748315236c9dd1a7013
artem21215 <13_22_54@mail.ru>
2021-05-20 03:33:53 +0700

    Add no-full basic

--- simpleAssembler/src/mySimpleAssebler.cpp
+++ simpleAssembler/src/mySimpleAssebler.cpp
@@ -1,27 +1,5 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-using namespace std;
-vector<short> mem(100);
-int sc_commandEncode (int command, int operand, int * value){
-    *value=0;
-    for (int i=0;i<7;++i){
-        int elem=operand&(1<<i);
-        if (elem!=0){
-            *value=*value|(1<<i);
-        }
-    }
-    for (int i=0;i<7;++i){
-        int elem=command&(1<<i);
-        if (elem!=0){
-            *value=*value|(1<<(i+7));
-        }
-    }
-    //*value|=(1<<14);
-    return 0;
-}
-void parse(string &spos, string &scom, string &sval){
+#include "mySimpleAssebler.h"
+void parse_AC(string &spos, string &scom, string &sval){
     int pos=(spos[0]-'0')*10+spos[1]-'0';
     int val=(sval[0]-'0')*10+sval[1]-'0';
     int com=0;
@@ -54,8 +32,6 @@ void parse(string &spos, string &scom, string &sval){
     else if (scom=="ADDC")
         com=65;
     else if (scom=="="){
-        cout << endl;
-        cout << sval << endl;
         int z=(sval[1]-'0')*16*16*16+(sval[2]-'0')*16*16+(sval[3]-'0')*16+(sval[4]-'0');
         if (sval[0]=='-'){
             mem[pos]=-z;
@@ -65,23 +41,28 @@ void parse(string &spos, string &scom, string &sval){
         return;
     }
     int coded;
-    //cout << pos << ' ' << com << ' ' << val << endl;
     if (sc_commandEncode(com,val,&coded)==0){
         mem[pos]=coded;
-        //cout << mem[pos] << ' ' << com << ' ' << val << endl;
     }
     else{
         cout << "error code" << endl;
         return;
     }
 }
-void SimpleBasic();
 int main(int argc, char *argv[]){
-    //SimpleBasic();
-    ifstream in(argv[1]);
+    string name_bsk;
+    for (int i=0;;++i){
+        if (argv[1][i]==0)
+            break;
+        name_bsk+=argv[1][i];
+    }
+    if (SimpleBasic(name_bsk)) {
+        cout << "Error translation from basic to accembler" << endl;
+        return 1;
+    }
+    ifstream in("myprog.sa");
     string line;
     string pos,com,val;
-    cout << endl;
     string word;
     vector<string> help;
     while (getline(in,line)){
@@ -104,23 +85,207 @@ int main(int argc, char *argv[]){
         com=help[1];
         val=help[2];
         help.clear();
-        parse(pos,com,val);
+        parse_AC(pos,com,val);
     }
     in.close();
-    for (auto v:mem)
-        cout << v << ' ';
 
     ofstream out("../input.o");
     for (int i=0;i<mem.size();++i){
         out << mem[i] << ' ';
     }
     out.close();
-   /* cout << endl;
-    cout << "name file: ";
-    cout << filename << endl;*/
+    cout << "Compile accepted!" << endl;
     return 0;
 }
-/*void SimpleBasic(){
 
-}*/
+int check_up(string spos, string scom, string sval){
+    if (scom=="REM")
+        return 0;
+    for (auto &v:spos){
+        if (v<'0' || v>'9') {
+            return 1;
+        }
+    }
+    for (auto &v:scom){
+        if (v<'A' || v>'Z') {
+            return 1;
+        }
+    }
+    if (scom=="END")
+        return 0;
+    for (auto &v:sval){
+        if (v>='a' && v<='z') {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int func_output(int prior){
+    prog[prior].num_mem=pointer++;
+    string buf;
+    if (pointer<10)
+        buf+='0';
+    buf+=to_string(prog[prior].num_mem);
+    buf+=' ';
+    buf+="WRITE ";
+    buf+=to_string(99-(prog[prior].val[0]-'A'));
+    cout << buf << endl;
+    ans.push_back(buf);
+    return 0;
+}
+
+int func_input(int prior){
+    prog[prior].num_mem=pointer++;
+    string buf;
+    if (pointer<10)
+        buf+='0';
+    buf+=to_string(prog[prior].num_mem);
+    buf+=' ';
+    buf+="READ ";
+    buf+=to_string(99-(prog[prior].val[0]-'A'));
+    cout << buf << endl;
+    ans.push_back(buf);
+    return 0;
+}
+
+int func_end(int prior){
+    prog[prior].num_mem=pointer++;
+    string buf;
+    if (pointer<10)
+        buf+='0';
+    buf+=to_string(prog[prior].num_mem);
+    buf+=' ';
+    buf+="HALT ";
+    cout << buf << endl;
+    ans.push_back(buf);
+    return 0;
+}
+
+int func_if_goto(int prior){
+    string buf;
+    prog[prior].num_mem=pointer++;
+    if (pointer<10)
+        buf+='0';
+    buf+=to_string(pointer-1);
+    buf+=" LOAD ";
+    string sval=prog[prior].val;
+    buf+=to_string(99-sval[0]+'A');
+    cout << buf << endl;
+    ans.push_back(buf);
+
+    buf.clear();
+    pointer++;
+    int i=sval.size()-1;
+    while (sval[i]>='0' && sval[i]<='9')
+        --i;
+    ++i;
+    buf+="~";
+    for (;i<sval.size();++i)
+        buf+=sval[i];
+    buf+='~';
+    if (pointer<10)
+        buf+='0';
+    buf+=to_string(pointer-1);
+    buf+=' ';
+    if (sval[1]=='=')
+        buf+="JZ ";
+    else if (sval[1]=='<')
+        buf+="JNEG";
+    else
+        return 1;
+    cout << buf << endl;
+    ans.push_back(buf);
+    return 0;
+}
+
+int func_goto(int prior){
+    prog[prior].num_mem=pointer++;
+    string buf;
+    buf+="~";
+    buf+=prog[prior].val;
+    buf+="~";
+    if (pointer<10)
+        buf+='0';
+    buf+=to_string(prog[prior].num_mem);
+    buf+=' ';
+    buf+="JUMP ";
+    cout << buf << endl;
+    ans.push_back(buf);
+    return 0;
+}
+
+int func_let(int prior){
+
+    return 0;
+}
+int parse_basic() {
+    for (auto &v:prog) {
+        //cout <<"##" << v.first << ' ' << v.second.num_mem << ' ' << v.second.comb << ' ' << v.second.val << endl;
+        if (v.second.comb == "INPUT") {
+            func_input(v.first);
+        }
+        else if (v.second.comb=="OUTPUT"){
+            func_output(v.first);
+        }
+        else if (v.second.comb=="IF"){
+            func_if_goto(v.first);
+        }
+        else if (v.second.comb=="GOTO"){
+            func_goto(v.first);
+        }
+        else if (v.second.comb=="END"){
+            func_end(v.first);
+        }
+        else if (v.second.comb=="LET"){
+            func_let(v.first);
+        }
+    }
+    return 0;
+}
+int calc_pos(string &help){
+    int pos=0;
+    for (int i=0;i<help.size();++i){
+        pos=pos*10+help[i]-'0';
+    }
+    return pos;
+}
+int SimpleBasic(string namebsk) {
+    //cout << namebsk << endl;
+    ifstream in(namebsk);
+    string line;
+    string pos, com, val;
+    string word;
+    vector<string> help;
+    while (!in.eof()) {
+        getline(in, line);
+        for (int i = 0; i < line.size(); ++i) {
+            if (line[i] != ' ' || help.size() == 2 && !word.empty()) {
+                word += line[i];
+            } else if (word.size() != 0) {
+                //cout << word << endl;
+                help.push_back(word);
+                word = "";
+            }
+        }
+        if (word.size() != 0) {
+            help.push_back(word);
+            word = "";
+        }
+        pos = help[0];
+        com = help[1];
+        val = help[2];
+        int position = calc_pos(help[0]);
+        prog[position].comb = com;
+        prog[position].val = val;
+        help.clear();
+        if (check_up(pos, com, val)) {
+            in.close();
+            return 1;
+        }
+    }
+    cout << endl;
+    parse_basic();
+    return 0;
+}
 
--- simpleAssembler/src/mySimpleAssebler.h
+++ simpleAssembler/src/mySimpleAssebler.h
@@ -4,5 +4,42 @@
 
 #ifndef ARCHITECH_MYSIMPLEASSEBLER_H
 #define ARCHITECH_MYSIMPLEASSEBLER_H
-
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+using namespace std;
+struct layers{
+    string comb="";
+    string val="";
+    int num_mem=0;
+};
+vector<short> mem(100);
+int pointer=0;
+map<int,layers> prog;
+vector<string> ans;
+void parse_AC(string &spos, string &scom, string &sval);
+int check_up(string spos, string scom, string sval);
+int func_input(int prior);
+int parse_basic(string spos, string scom, string sval);
+int func_output(int prior);
+int func_if_goto(int prior);
+int SimpleBasic(string namebsk);
+int sc_commandEncode (int command, int operand, int * value){
+    *value=0;
+    for (int i=0;i<7;++i){
+        int elem=operand&(1<<i);
+        if (elem!=0){
+            *value=*value|(1<<i);
+        }
+    }
+    for (int i=0;i<7;++i){
+        int elem=command&(1<<i);
+        if (elem!=0){
+            *value=*value|(1<<(i+7));
+        }
+    }
+    return 0;
+}
 #endif //ARCHITECH_MYSIMPLEASSEBLER_H
